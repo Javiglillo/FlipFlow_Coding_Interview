317045d8a3fe40f5847742781988e09a0bf0b66d
javiglillo <jgl260200@gmail.com>
2023-11-16 12:52:24 +0100

    Comandos creados

---
+++ app/Console/Commands/SaveProductListCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SaveProductListCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'save-product-list';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Store products from the url in the database';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
---
+++ app/Console/Commands/ShowProductListCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ShowProductListCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'show-product-list';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Show products from the url in json format';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
