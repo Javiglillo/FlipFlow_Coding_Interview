f800c956401d156b431b98e35e091729a9979005
javiglillo <jgl260200@gmail.com>
2023-11-18 14:15:35 +0100

    Extraido metodo de scraping

--- app/Services/ScraperProductsService.php
+++ app/Services/ScraperProductsService.php
@@ -9,7 +9,7 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class ScraperProductsService {
     
-    public function saveProducts($url){
+    public function getProducts($url){
         $browser = new HttpBrowser(HttpClient::create());
         $browser->request('GET', $url);
         $response = $browser->getResponse();
@@ -18,14 +18,22 @@ class ScraperProductsService {
         $crawler = new Crawler($body);
         $basePath = $crawler->filter('base')->attr('href');
         $productNodes = $crawler->filter('ul.product-card-list__list > li')->slice(0,5);
+        $products = [];
 
-        $productNodes->each(function (Crawler $productNode) use  ($basePath){
+        $productNodes->each(function (Crawler $productNode) use ($basePath, &$products){
             $product = new Product();
             $product->name = $productNode->filter('.product-card__title-link')->text();
             $product->price = $productNode->filter('.product-card__parent')->attr('app_price');
             $product->image_url = $productNode->filter('.product-card__media-link > img')->attr('src');
             $product->url = $basePath . $productNode->filter('.product-card__media > a')->attr('href');
-            $product->save();
+            $products[] = $product->toArray();
         });
+
+        return $products;
+    }
+
+    public function saveProducts($url){
+        $products = $this->getProducts($url);
+        Product::insert($products);
     }
 }
