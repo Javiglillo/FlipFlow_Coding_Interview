acc67ede2cd08dfcf0d58c3c9ebdaa1c4f2cca63
javiglillo <jgl260200@gmail.com>
2023-11-18 18:10:44 +0100

    Tratamiento de errores y validaciones en scraping realizadas

--- app/Services/ScraperProductsService.php
+++ app/Services/ScraperProductsService.php
@@ -3,6 +3,7 @@
 namespace App\Services;
 
 use App\Models\Product;
+use Exception;
 use Symfony\Component\BrowserKit\HttpBrowser;
 use Symfony\Component\HttpClient\HttpClient;
 use Symfony\Component\DomCrawler\Crawler;
@@ -12,29 +13,37 @@ class ScraperProductsService {
     public function __construct(private ProductService $productService){}
     
     public function getProducts($url, $action){
-        $browser = new HttpBrowser(HttpClient::create());
-        $browser->request('GET', $url);
-        $response = $browser->getResponse();
-        $body = $response->getContent();
+        try{
+            $browser = new HttpBrowser(HttpClient::create());
+            $browser->request('GET', $url);
+            $response = $browser->getResponse();
 
-        $crawler = new Crawler($body);
-        $basePath = $crawler->filter('base')->attr('href');
-        $productNodes = $crawler->filter('ul.product-card-list__list > li')->slice(0,5);
-        $products = [];
+            if($response->getStatusCode() === 200){
+                $body = $response->getContent();
+    
+                $crawler = new Crawler($body);
+                $basePath = $crawler->filter('base')->attr('href');
+                $productNodes = $crawler->filter('ul.product-card-list__list > li')->slice(0,5);
+                $products = [];
 
-        $productNodes->each(function (Crawler $productNode) use ($basePath, &$products){
-            $product = new Product();
-            $product->name = $productNode->filter('.product-card__title-link')->text();
-            $product->price = $productNode->filter('.product-card__parent')->attr('app_price');
-            $product->image_url = $productNode->filter('.product-card__media-link > img')->attr('src');
-            $product->url = $basePath . $productNode->filter('.product-card__media > a')->attr('href');
-            $products[] = $product->toArray();
-        });
-
-        if($action === 'save'){
-            $this->productService->saveProducts($products);
-        } elseif ($action === 'show'){
-            $this->productService->showProducts($products);
+                $productNodes->each(function (Crawler $productNode) use ($basePath, &$products){
+                    $product = new Product();
+                    $product->name = $productNode->filter('.product-card__title-link')->text();
+                    $product->price = $productNode->filter('.product-card__parent')->attr('app_price');
+                    $product->image_url = $productNode->filter('.product-card__media-link > img')->attr('src');
+                    $product->url = $basePath . $productNode->filter('.product-card__media > a')->attr('href');
+                    $products[] = $product->toArray();
+                });
+        
+                if($action === 'save'){
+                    $this->productService->saveProducts($products);
+                } elseif ($action === 'show'){
+                    $this->productService->showProducts($products);
+                }
+            }
+        } catch(Exception $e){
+            echo 'Caught exception: ' . $e->getMessage() . PHP_EOL . 'This may be because the url provided is not correct';
         }
+
     }
 }
